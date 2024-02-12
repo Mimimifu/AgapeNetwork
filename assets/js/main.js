885922e5ad3b8b7ec368051d94a0b13675077e06
Mimimifu <130190513+Mimimifu@users.noreply.github.com>
2024-02-12 13:29:05 -0300

    all

---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+import storage from '/assets/js/storage.js'
---
+++ assets/js/storage.js
@@ -0,0 +1,33 @@
+import mongodb from 'https://unpkg.com/mongodb@5.6.0/lib/index.js'
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://lbbeazrr:UHI2aew6bYyvOR7d@cluster0.22jsnue.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+class storage {
+
+
+
+}
