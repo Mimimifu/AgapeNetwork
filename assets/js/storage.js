fc40db9b0e3f4a39d0ba2d8f564b70636e6a5d19
Mimimifu <130190513+Mimimifu@users.noreply.github.com>
2024-02-12 16:37:25 -0300

    storage

--- assets/js/storage.js
+++ assets/js/storage.js
@@ -2,5 +2,20 @@
 class storage {
 
 
+    set(key,value){
+        localStorage.setItem(key,value)
+    }
+
+    get(key){
+        localStorage.getItem(key)
+    }
+
+    save(){
+
+    }
+
+    load(){
+
+    }
 
 }
